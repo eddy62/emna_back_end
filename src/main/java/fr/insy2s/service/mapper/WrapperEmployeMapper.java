fd51fb40c1306a14a5ab3e9a04458236e5c0b112
Abuwe_Mireille <charline.abuwe@gmail.com>
2020-08-03 20:42:55 +0200

    wrapperEmploye

---
+++ src/main/java/fr/insy2s/service/mapper/WrapperEmployeMapper.java
@@ -0,0 +1,19 @@
+package fr.insy2s.service.mapper;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Mapper for the entity Employe, Adresse, StatutEmploye, Societe, InfoEntreprise and the DTO WrapperEmploye.
+ *
+ */
+@Service
+public class WrapperEmployeMapper {
+    
+    //builder WrapperEmploye
+    //WrapperEmploye to EmployeDTO
+    //WrapperEmploye to AdresseDTO
+    //WrapperEmploye to StatutEmployeDTO
+    //WrapperEmploye to SocieteDTO
+    //WrapperEmploye to InfoEntreprise
+
+}
