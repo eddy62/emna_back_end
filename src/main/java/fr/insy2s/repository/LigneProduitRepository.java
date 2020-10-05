878eb6b8d9fdb20d8f9db95c7b63d69e413480dd
Nassim Boubziz <boubziznassim14@gmail.com>
2020-10-05 14:25:12 +0200

    [#1471][FIX][NB] invoice module rephasing

--- src/main/java/fr/insy2s/repository/LigneProduitRepository.java
+++ src/main/java/fr/insy2s/repository/LigneProduitRepository.java
@@ -3,7 +3,10 @@ package fr.insy2s.repository;
 import fr.insy2s.domain.LigneProduit;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Spring Data  repository for the LigneProduit entity.
@@ -11,4 +14,8 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface LigneProduitRepository extends JpaRepository<LigneProduit, Long> {
+
+    @Query("select l from LigneProduit l " +
+        "where l.facture.id =: idFacture")
+    List<LigneProduit> getLigneProduitByIdFacture(@Param("idFacture") Long idFacture);
 }
