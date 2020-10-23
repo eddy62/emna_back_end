7685b437a220055904a48d80406d877b03e925be
matthieu <matthieu_waroux59@hotmail.com>
2020-10-23 12:07:26 +0200

    Add Features #915 : Add a service in the back end to archive a quote

---
+++ src/main/java/fr/insy2s/utils/QuoteStateConstants.java
@@ -0,0 +1,8 @@
+package fr.insy2s.utils;
+
+public final class QuoteStateConstants {
+
+    public static final Long DEVIS_NON_ARCHIVE = 1L;
+
+    public static final Long DEVIS_ARCHIVE     = 2L;
+}
